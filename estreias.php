2c79ffad1eb025e6a2b9547b4f3398c80190d1af
GUSTAVO PIRES TRENTINI <2650831823031@lab.fatecsaoroque.edu.br>
2019-11-07 20:27:04 -0300

    Conjunto da obra

--- estreias.php
+++ estreias.php
@@ -12,7 +12,8 @@
             <div class="card-body d-flex flex-column">
                 <h4 class="card-title font-weight-bold text-center">John Wick 3</h4>
                 <h6 class="card-subtitle mb-2 text-muted text-center"><?= __('Sinopse e Detalhes', 'Synopsis and Details') ?></h6>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+                <p><?= __('O lendário John Wick luta para sair de Nova York quando um contrato de 14 milhões de dólares faz dele o alvo dos maiores assassinos do mundo.','The legendary John Wick struggles to leave New York when a $14 million contract makes him the target of the worlds greatest killers...') ?>
+                </p>
                 <ul>
                     <li><?= __('Estreia', 'Premiere') ?>: <?= __('16 de Maio 2019', 'May 16, 2019') ?></li>
                     <li><?= __('Direção', 'Direction') ?>: Chad Stahelski</li>
@@ -29,7 +30,7 @@
             <div class="card-body d-flex flex-column">
                 <h4 class="card-title font-weight-bold text-center">Hellboy</h4>
                 <h6 class="card-subtitle mb-2 text-muted text-center"><?= __('Sinopse e Detalhes', 'Synopsis and Details') ?></h6>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco lorem.</p>
+                <p><?= __('Perto do fim da Segunda Guerra Mundial, os nazistas se mostram dispostos a lançar mão de magia negra para derrotar seus inimigos.','Near the end of World War II, the Nazis are willing to hand up black magic to defeat their enemies.') ?></p>
                 <ul>
                     <li><?= __('Estreia', 'Premiere') ?>: <?= __('16 de Maio 2019', 'May 16, 2019') ?></li>
                     <li><?= __('Direção', 'Direction') ?>: Neil Marshall</li>
@@ -46,7 +47,7 @@
             <div class="card-body d-flex flex-column">
                 <h4 class="card-title font-weight-bold text-center">Annabelle 3</h4>
                 <h6 class="card-subtitle mb-2 text-muted text-center"><?= __('Sinopse e Detalhes', 'Synopsis and Details') ?></h6>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco lorem ipsum.</p>
+                <p><?= __('Os investigadores paranormais Ed e Lorraine Warren mantêm a boneca Annabelle trancada em uma sala de artefatos em sua casa.','Paranormal investigators Ed and Lorraine Warren keep the Annabelle doll locked in an artifact room in their home.')?></p>
                 <ul>
                     <li><?= __('Estreia', 'Premiere') ?>: <?= __('27 de Junho 2019', 'June 27, 2019') ?></li>
                     <li><?= __('Direção', 'Direction') ?>: Gary Dauberman</li>
@@ -64,7 +65,7 @@
             <div class="card-body d-flex flex-column">
                 <h4 class="card-title font-weight-bold text-center"><?= __('Os Novos Mutantes', 'The New Mutants') ?></h4>
                 <h6 class="card-subtitle mb-2 text-muted text-center"><?= __('Sinopse e Detalhes', 'Synopsis and Details') ?></h6>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco lorem ipsum amet.</p>
+                <p><?= __('Cinco jovens mutantes acabam de descobrir suas habilidades. Presos contra sua vontade em um local de segurança máxima, eles precisa lutar para escapar do inimigo e de seus pecados do passado. ','Five young mutants have just discovered their skills. Trapped against their will in a maximum security location, they need to fight to escape the enemy and their sins from the past. ')?></p>
                 <ul>
                     <li><?= __('Estreia', 'Premiere') ?>: <?= __('01 de Agosto 2019', 'August 01, 2019') ?></li>
                     <li><?= __('Direção', 'Direction') ?>: Josh Boone</li>
@@ -81,7 +82,7 @@
             <div class="card-body d-flex flex-column">
                 <h4 class="card-title font-weight-bold text-center"><?= __('Velozes e Furiosos: Hobbs e Shaw', 'Fast and Furious - Hobbs e Shaw') ?></h4>
                 <h6 class="card-subtitle mb-2 text-muted text-center"><?= __('Sinopse e Detalhes', 'Synopsis and Details') ?></h6>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco. Lorem ipsum.</p>
+                <p><?= __('O corpulento policial Luke Hobbs se junta ao fora da lei Deckard Shaw para combater um terrorista geneticamente melhorado que tem força sobre-humana.','Burly police officer Luke Hobbs joins the outlaw Deckard Shaw to fight a genetically enhanced terrorist who has superhuman strength.') ?></p>
                 <ul>
                     <li><?= __('Estreia', 'Premiere') ?>: <?= __('01 de Agosto 2019', 'August 01, 2019') ?></li>
                     <li><?= __('Direção', 'Direction') ?>: David Leitch</li>
@@ -98,7 +99,7 @@
             <div class="card-body d-flex flex-column">
                 <h4 class="card-title font-weight-bold text-center"><?= __('Coringa', 'Joker') ?></h4>
                 <h6 class="card-subtitle mb-2 text-muted text-center"><?= __('Sinopse e Detalhes', 'Synopsis and Details') ?></h6>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+                <p><?= __('O comediante falido Arthur Fleck encontra violentos bandidos pelas ruas de Gotham City. Desconsiderado pela sociedade, Fleck começa a ficar louco e se transforma no criminoso conhecido como Coringa.','Failed comedian Arthur Fleck encounters violent thugs through the streets of Gotham City. Disregarded by society, Fleck begins to go crazy and becomes the criminal known as the Joker.')?></p>
                 <ul>
                     <li><?= __('Estreia', 'Premiere') ?>: <?= __('03 de Outubro 2019', 'October 03, 2019') ?></li>
                     <li><?= __('Direção', 'Direction') ?>: Todd Phillips</li>
@@ -115,7 +116,7 @@
             <div class="card-body d-flex flex-column">
                 <h4 class="card-title font-weight-bold text-center"><?= __('Projeto Gemini', 'Gemini Man') ?></h4>
                 <h6 class="card-subtitle mb-2 text-muted text-center"><?= __('Sinopse e Detalhes', 'Synopsis and Details') ?></h6>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+                <p><?= __('Henry Brogan é um assassino de elite que se torna o alvo de um agente misterioso que aparentemente pode prever todos os seus movimentos. Ele logo descobre que o homem que está tentando matá-lo é uma versão mais jovem.','Henry Brogan is an elite assassin who becomes the target of a mysterious agent who can apparently predict all his movements. He soon discovers that the man who is trying to kill him is a younger version.')?></p>
                 <ul>
                     <li><?= __('Estreia', 'Premiere') ?>: <?= __('10 de Outubro 2019', 'October 10, 2019') ?></li>
                     <li><?= __('Direção', 'Direction') ?>: Ang Lee</li>
@@ -132,7 +133,7 @@
             <div class="card-body d-flex flex-column">
                 <h4 class="card-title font-weight-bold text-center">IT 2</h4>
                 <h6 class="card-subtitle mb-2 text-muted text-center"><?= __('Sinopse e Detalhes', 'Synopsis and Details') ?></h6>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco Lorem ipsum.</p>
+                <p><?= __('Vinte e sete anos depois dos eventos que chocaram os adolescentes que faziam parte do Clube dos Perdedores, os amigos realizam uma reunião. No entanto, o reencontro se torna uma verdadeira e sangrenta batalha quando Pennywise, o palhaço, retorna.','Twenty-seven years after the events that shocked the teenagers who were part of the Loserclub, friends hold a meeting. However, the reunion becomes a true and bloody battle when Pennywise, the clown, returns.')?></p>
                 <ul>
                     <li><?= __('Estreia', 'Premiere') ?>: <?= __('05 de Setembro 2019', 'September 05, 2019') ?></li>
                     <li><?= __('Direção', 'Direction') ?>: Andy Muschietti</li>
@@ -149,7 +150,7 @@
             <div class="card-body d-flex flex-column">
                 <h4 class="card-title font-weight-bold text-center"><?= __('Star Wars- A Ascensão Skiwalker', 'Trailer - The Rise of Skywalker') ?></h4>
                 <h6 class="card-subtitle mb-2 text-muted text-center"><?= __('Sinopse e Detalhes', 'Synopsis and Details') ?></h6>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco Lorem Ipsum.</p>
+                <p><?= __('Lucasfilm e o diretor J.J. Abrams juntam forças mais uma vez para levar os espectadores a uma jornada épica em uma galáxia muito, muito distante em Star Wars: A Ascensão Skywalker, a fascinante conclusão da saga Skywalker.','Lucasfilm and director J.J. Abrams join forces once again to lead viewers on an epic journey into a galaxy far, far away in Star Wars: The Skywalker Rise, the fascinating conclusion of the Skywalker saga.')?></p>
                 <ul>
                     <li><?= __('Estreia', 'Premiere') ?>: <?= __('19 de Dezembro 2019', 'December 19, 2019') ?></li>
                     <li><?= __('Direção', 'Direction') ?>: J.J. Abrams</li>
