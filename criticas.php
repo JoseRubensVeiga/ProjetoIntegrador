11202f009d66d095e1cb5dd8f25d8d5ce209a2f3
GUSTAVO PIRES TRENTINI <2650831823031@lab.fatecsaoroque.edu.br>
2019-11-07 20:04:41 -0300

    Remoção dos Lorem Ipsum da página de críticas.

--- criticas.php
+++ criticas.php
@@ -7,7 +7,7 @@
             <img src="images/Aquaman.jpg" class="card-img-top" alt=" imagem do aquaman">
             <div class="card-body">
             <h5>Aquaman</h5>
-                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In enim mi, semper eu fringilla eu.</p>
+                <p class="card-text"><?= __('Aquaman é considerado um dos filmes mais emocionantes da DC nos últios 10 anos.', 'Aquaman is considered one of DC most exciting movies in the last 10 years.') ?></p>
                 <img src="images/star.png" class="card-img-botton" alt="ícone 4 estrelas">
 
             </div>
@@ -18,7 +18,8 @@
             <img src="images/Bumblebee.jpg" class="card-img-top" alt=" imagem do bumblebee">
             <div class="card-body">
             <h5>Bumblebee</h5>
-                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In enim mi, semper eu fringilla eu.</p>
+                <p class="card-text"><?= __('Um dos filmes com a pior bilheteria dos últimos tempos segundo a globo.', '
+One of the worst-grossing movies of all time around the globe.') ?></p>
                 <img src="images/star.png" class="card-img-botton" alt="ícone 4 estrelas">
 
             </div>
@@ -29,7 +30,7 @@
             <img src="images/capitamarvel.jpg" class="card-img-top" alt="imagem da capitã marvel">
             <div class="card-body">
             <h5><?= __('Capitã Marvel', 'Captain Marvel') ?></h5>
-                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In enim mi, semper eu fringilla eu.</p>
+                <p class="card-text"><?= __('O filme com maior público feminino nos últimos 20 anos, um sucess de bilheteria', 'The biggest female audience film in the last 20 years, a box office success') ?></p>
                 <img src="images/star.png" class="card-img-botton" alt="ícone 4 estrelas">
 
             </div>
@@ -40,7 +41,8 @@
             <img src="images/Dumbo.jpg" class="card-img-top" alt="imagem do dumbo">
             <div class="card-body">
             <h5>Dumbo</h5>
-                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In enim mi, semper eu fringilla eu.</p>
+                <p class="card-text"><?= __('Apesar do título carregar o nome do elefante, o filme de Burton com roteiro de Ehren Kruger decide passar boa parte de seu tempo desenvolvendo a história de humanos antes de sequer apresentar o animal.', 'Although the title bears the elephant name, Burton screenplay 
+                by Ehren Kruger decides to spend much of his time developing human history before he even introduces the animal.') ?></p>
                 <img src="images/star.png" class="card-img-botton" alt="ícone 4 estrelas">
 
             </div>
@@ -51,7 +53,7 @@
             <img src="images/homemaranha.jpg" class="card-img-top" alt="imagem do homem aranha">
             <div class="card-body">
             <h5><?= __('Homem-Aranha no Aranhaverso', 'Spider-Man into The Spider-Verse') ?></h5>
-                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In enim mi, semper eu fringilla eu.</p>
+                <p class="card-text"><?= __('Reconhecendo a ciência que o público já tem de sua história de fundo, essa introdução ocorre da forma mais simplificada possível, sequer mencionando o tio Ben.', 'Recognizing the public awareness of his backstory, this introduction takes place as simply as possible, not even mentioning Uncle Ben.') ?></p>
                 <img src="images/star.png" class="card-img-botton" alt="ícone 4 estrelas">
 
             </div>
@@ -62,7 +64,7 @@
             <img src="images/maquinasmortais.jpg" class="card-img-top" alt=" capa do filme máquinas mortais">
             <div class="card-body">
             <h5><?= __('Máquinas Mortais', 'Mortal Machines') ?></h5>
-                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In enim mi, semper eu fringilla eu.</p>
+                <p class="card-text"><?= __('Baseado no primeiro livro do universo homônimo criado por Philip Reeve, Máquinas Mortais nos apresenta a um mundo distópico que, milhares de anos antes, havia sido devastado.', 'Based on the first book in the namesake universe created by Philip Reeve, Deadly Machines introduces us to a dystopian world that, thousands of years earlier, had been devastated.') ?></p>
                 <img src="images/star.png" class="card-img-botton" alt="ícone 4 estrelas">
 
             </div>
@@ -73,9 +75,8 @@
             <img src="images/minhavidaemarte.jpg" class="card-img-top" alt="imagem de dois personagens principais do filme minha vida em marte">
             <div class="card-body">
             <h5><?= __('Minha Vida em Marte', 'My Life on Mars') ?></h5>
-                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In enim mi, semper eu fringilla eu.</p>
+                <p class="card-text"><?= __('Ao final do longa anterior, Fernanda (Mônica Martelli) parecia ter encontrado o parceiro para a vida toda em Tom (Marcos Palmeira). O enredo de Minha Vida em Marte tem seu início anos depois, com os dois casados e já dividindo seu espaço com uma filha pequena', 'At the end of the previous feature, Fernanda (Mônica Martelli) seemed to have found her life partner in Tom (Marcos Palmeira). The plot of My Life on Mars begins years later, with the two married and already sharing their space with a young daughter') ?></p>
                 <img src="images/star.png" class="card-img-botton " alt="ícone 4 estrelas">
-
             </div>
         </div>
     </div>
